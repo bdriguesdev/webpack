e0c799573c36f2c906e3bcb794a7437ef554fdb6
bdriguesdev <bdriguesdev@gmail.com>
2020-01-19 15:09:57 -0300

    Added the project

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import { Header } from './app/header';let header = new Header();let firstHeading = header.getFirstHeading();console.log(firstHeading);
+import './styles/scss/main.scss';
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: [/.js$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {                
+                test: [/.css$|.scss$/],                
+                use:[                    
+                 'style-loader',                  
+                 'css-loader',
+                 'sass-loader'
+                ]            
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack 4 Starter',
+            template: './src/index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        })
+    ]
+};
